447606552e8ac40d6940de64bff1ddba241b2416
vidal1101 <rodrigovidal278@gmail.com>
2022-05-21 00:30:40 -0600

    tercera prueba de descarga del documento

--- assets/js/myfuntion.js
+++ assets/js/myfuntion.js
@@ -3,7 +3,5 @@ function donwloadCV() {
 
     let file = document.getElementById("download_cv");
 
-    document.body.appendChild(file);
-    file.click;
-    //document.body.removeChild(file);
+
 }
